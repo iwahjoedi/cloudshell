18345bbd752b54d985640f63f29e81641edd428a
Indra wahjoedi <indra.wahjoedi@gmail.com>
2021-03-12 23:36:56 +0700

    update setup.sh

---
+++ setup.sh
@@ -0,0 +1,21 @@
+!#/bin/sh
+
+echo -e "Downloading Neovim...\n"
+wget https://github.com/neovim/neovim/releases/download/v0.4.4/nvim-linux64.tar.gz
+
+echo -e "Extracting Neovim...\n"
+tar zxvf nvim-linux64.tar.gz
+yes | rm -rf nvim-linux64.tar.gz
+
+echo -e "Renaming dot-files...\n"
+cat dot-bashrc >> $HOME/.bashrc
+mv dot-zshrc $HOME/.zshrc
+cat dot-tmux.conf >> .tmux.conf
+
+echo -e "Installing Zsh...\n"
+sudo apt install zsh
+
+echo -e "Installing Oh-MyZsh...\n"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
